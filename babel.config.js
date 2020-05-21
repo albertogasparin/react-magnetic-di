252b34a30f284cb72d19b99f252c28324bab678f
Alberto Gasparin <albertogasparin@gmail.com>
2020-05-21 22:47:22 +1000

    Add build configs

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,11 @@
 
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      { modules: false, exclude: ['transform-typeof-symbol'] },
-    ],
+    ['@babel/preset-env', { modules: false }],
     '@babel/preset-react',
     '@babel/preset-flow',
   ],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties'],
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    './src/babel',
-  ],
+  plugins: ['@babel/plugin-proposal-class-properties', './src/babel'],
   env: {
     test: {
       presets: ['@babel/preset-env'],
