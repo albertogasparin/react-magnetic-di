7ae461587eb38559bb070d370bf706b62608bab4
Vitalii Blinovskov <vblinovskov@atlassian.com>
2020-06-19 12:47:58 +1000

    fix withDi HOC types

--- examples/flow/components/label.js
+++ examples/flow/components/label.js
@@ -1,13 +1,15 @@
 // @flow
-import React, { useState } from 'react';
+import React, { useState, type Node } from 'react';
 import { di } from 'react-magnetic-di';
 
 export function useTheme() {
   return useState<any>({ color: '#333' });
 }
 
-export function Label() {
+export type LabelProps = {| children?: Node |};
+
+export function Label({ children = 'Label' }: LabelProps) {
   di(useTheme);
   const [style] = useTheme();
-  return <div style={style}>Label</div>;
+  return <div style={style}>{children}</div>;
 }
