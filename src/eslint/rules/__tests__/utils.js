c2c56740f8177e6570ca12969406d72440b12912
Alberto Gasparin <albertogasparin@gmail.com>
2020-07-08 09:36:49 +1000

    Fix ESLint exhaustive rule with component props

--- src/eslint/rules/__tests__/utils.js
+++ src/eslint/rules/__tests__/utils.js
@@ -1,4 +1,5 @@
 export const genericCases = [
+  // should not trigger when no di imported
   `
     import React from 'react';
     import { Query } from 'react-apollo';
@@ -7,5 +8,63 @@ export const genericCases = [
       const tree = shallow(<Query />);
       expect(tree).toMatchSnapshot();
     });
+  `,
+
+  // should support functional components
+  `
+    import React from 'react';
+    import { di } from 'react-magnetic-di';
+    import { ImportedComponent, useImportedHook } from './imported';
+
+    const ModuleComponent = () => null;
+    const useModuleHook = () => null;
+
+    function MyComponent ({ PropComponent, usePropHook }) {
+      di(ImportedComponent, ModuleComponent);
+      di(useImportedHook, useModuleHook);
+      
+      const LocalComponent = () => null;
+      const useLocalHook = () => null;
+      
+      useImportedHook();
+      useModuleHook();
+      usePropHook();
+      useLocalHook();
+
+      return (
+        <>
+          <ImportedComponent />
+          <ModuleComponent />
+          <PropComponent />
+          <LocalComponent />
+        </>
+      );
+    }
+  `,
+
+  // should support class components
+  `
+    import React, { Component } from 'react';
+    import { di } from 'react-magnetic-di';
+    import { ImportedComponent, useImportedHook } from './imported';
+
+    const ModuleComponent = () => null;
+    
+    class MyComponent extends Component {
+      render() {
+        di(ImportedComponent, ModuleComponent);
+        const { PropComponent } = this.props;
+        const LocalComponent = () => null;
+
+        return (
+          <>
+            <ImportedComponent />
+            <ModuleComponent />
+            <PropComponent />
+            <LocalComponent />
+          </>
+        );
+      }
+    }
   `,
 ];
