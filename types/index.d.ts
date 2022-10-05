7f307f4b75925e6eb21b78a27ec7f34e9ad4f989
Pavlo-Bohdan Ravliv <ravliv.pavlobohdan@gmail.com>
2022-10-05 11:30:14 +0200

    update implementation of injectable

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,24 @@ declare module 'react-magnetic-di' {
 
   type Dependency = Function;
 
+  export type DeepPartial<Type> = Type extends DeepPartialFunction<Type>
+    ? DeepPartialFunction<Type>
+    : Type extends Array<infer InferredArrayMember>
+    ? DeepPartialArray<InferredArrayMember>
+    : Type extends object
+    ? DeepPartialObject<Type>
+    : Type | undefined;
+
+  interface DeepPartialArray<Type> extends Array<DeepPartial<Type>> {}
+
+  type DeepPartialObject<Type> = {
+    [Key in keyof Type]?: DeepPartial<Type[Key]>;
+  };
+
+  type DeepPartialFunction<Type> = Type extends (...args: any) => any
+    ? (...args: Parameters<Type>) => DeepPartialObject<ReturnType<Type>>
+    : Type;
+
   class DiProvider extends Component<
     {
       use: Dependency[];
@@ -21,7 +39,7 @@ declare module 'react-magnetic-di' {
   /** @deprecated use injectable instead */
   function mock<T extends Dependency>(original: T, mock: T): T;
 
-  function injectable<T extends Dependency>(from: T, implementation: T): T;
+  function injectable<T extends Dependency>(from: T, implementation: DeepPartial<T>): T;
 
   function di(...dependencies: Dependency[]): void;
   /** allow using di without Babel */
